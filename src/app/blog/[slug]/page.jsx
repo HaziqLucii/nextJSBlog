e45c5646b786147ffd9c41643feb99211cce5449
haziq <ikhmal@plisca.com.my>
2024-03-31 21:54:23 +0800

    3:00:50

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -27,32 +27,24 @@ const SinglePostPage = async ({ params }) => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/19294343/pexels-photo-19294343/free-photo-of-pink-car.jpeg"
-          alt=""
-          fill
-          className={styles.img}
-        />
+        <Image src={post.img} alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
+        <h1 className={styles.title}>{post?.title}</h1>
         <div className={styles.detail}>
-          <Image
-            src="https://images.pexels.com/photos/19294343/pexels-photo-19294343/free-photo-of-pink-car.jpeg"
-            alt=""
-            className={styles.avatar}
-            width={50}
-            height={50}
-          />
-          <Suspense fallback={<Skeleton />}>
-            <PostUser userId={post.userId} />
-          </Suspense>
+          {post && (
+            <Suspense fallback={<Skeleton />}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.01.2024</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
           </div>
         </div>
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post?.description}</div>
       </div>
     </div>
   );
--- src/components/postUser/PostUser.jsx
+++ src/components/postUser/PostUser.jsx
@@ -1,5 +1,6 @@
 import { getUser } from "@/lib/data";
 import styles from "./postuser.module.css";
+import Image from "next/image";
 
 // const getUser = async (userId) => {
 //   const res = await fetch(
@@ -18,8 +19,17 @@ const PostUser = async ({ userId }) => {
   const user = await getUser(userId);
   return (
     <div className={styles.container}>
-      <span className={styles.title}>Author</span>
-      <span className={styles.username}>{user.name}</span>
+      <Image
+        src={user.img ? user.img : "/noavatar.png"}
+        alt=""
+        className={styles.avatar}
+        width={50}
+        height={50}
+      />
+      <div className={styles.texts}>
+        <span className={styles.title}>Author</span>
+        <span className={styles.username}>{user.username}</span>
+      </div>
     </div>
   );
 };
